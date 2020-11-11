983a941b8368132fe6fdb262df54c3599b1335a3
maxime2103i <48186655+maxime2103i@users.noreply.github.com>
2020-11-11 11:51:53 +0100

    feat(palindrome): add palindrome def and  show result def

---
+++ 1_Palindrome/palindrome.rb
@@ -0,0 +1,19 @@
+
+puts "Enter a word"
+word = gets.chomp
+
+# verify if the word is equal to the reverse same word
+def is_palindrome word
+    return word == word.reverse
+end
+
+# show result of def is_palindrome
+def show_result word
+    if is_palindrome(word)
+        puts "The word " + word + " is a palindrome"
+    else
+        puts "Le mot " + word + " is not a palindrome"
+    end
+end
+
+show_result(word)
