0b43824ea6842d330698f2c0b355d3c8bbdfbdc0
maxime2103i <48186655+maxime2103i@users.noreply.github.com>
2020-11-15 16:01:16 +0100

    feat(checksiretnumber): finishing exercise 6

--- ruby/exo_6.rb
+++ ruby/exo_6.rb
@@ -1,1 +1,30 @@
-﻿
+
+puts "Enter your siret number"
+SIRET_number = gets.chomp
+
+def verify_siret siret_number
+    siret_number = siret_number.split('')
+    if siret_number.length == 14
+        sum = 0
+        count = 0
+
+        # excute th Luhn algorithm
+        for i in siret_number do
+            i = i.to_i
+            if count % 2 != 0
+                i *= 2
+                if i > 9
+                    i -= 9
+                end
+                sum += i
+            else
+                sum += i
+            end
+            count += 1
+        end
+        
+        return sum % 10 == 0
+    end
+end
+
+puts verify_siret(SIRET_number) ? "ok" : "not ok"
